5ec18f0af0ad71c103dc05c242dd764b5320669d
ADITYA RAWAT <adityarawat240@gmail.com>
2025-10-20 18:46:10 +0530

    snackbar/toast

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:second_flutter_project/widgets/container_widget.dart';
 import 'package:second_flutter_project/widgets/listGrid_widget.dart';
 import 'package:second_flutter_project/widgets/row_col_widget.dart';
+import 'package:second_flutter_project/widgets/snackbar_widget.dart';
 import 'package:second_flutter_project/widgets/textButton.dart';
 
 void main() =>runApp(MyApp());
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.deepOrange
       ),
-      home: ListgridWidget()
+      home: SnackbarWidget()
     );
   }
 }
---
+++ lib/widgets/snackbar_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SnackbarWidget extends StatelessWidget {
+  const SnackbarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Snackbar or should i say toast/sonner")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            final Snackbar = SnackBar(
+              content: Text("this is a mafaka toast"),
+              backgroundColor: Colors.amber,
+              showCloseIcon: false,
+              duration: Duration(seconds: 4),
+              behavior: SnackBarBehavior.floating,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+              action: SnackBarAction(label: "Undo", onPressed: (){print("This will help in undo option");}, textColor: Colors.blue,),
+            );
+            ScaffoldMessenger.of(
+              context,
+            ).hideCurrentSnackBar(reason: SnackBarClosedReason.hide);
+            ScaffoldMessenger.of(context).showSnackBar(Snackbar);
+          },
+          child: Text("Show Toast"),
+        ),
+      ),
+    );
+  }
+}
