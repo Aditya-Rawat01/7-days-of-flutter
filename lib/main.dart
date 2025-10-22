d6ecf005c18ee067ea96296dee28b4735c8378bc
ADITYA RAWAT <adityarawat240@gmail.com>
2025-10-22 16:04:55 +0530

    dropdown list widget

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
         primaryColor: Colors.purple
       ),
-      home: BottomnavigationWidget()
+      home: DropdownlistWidget()
     );
   }
 }
--- lib/widgets/dropDownList_widget.dart
+++ lib/widgets/dropDownList_widget.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
-class DropdownlistWidget extends StatelessWidget {
+class DropdownlistWidget extends StatefulWidget {
   const DropdownlistWidget({super.key});
-
+ @override
+ State<DropdownlistWidget> createState() => _dropDownState();
+}
+class _dropDownState extends State<DropdownlistWidget> {
+  String selectedValue="a";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("DropDown List Widget"),),
       body: Center(
-        
+        child: DropdownButton<String>(
+          value: selectedValue,
+          // hint: const Text("Select a value"),
+          items: <String>["a","b","c","d"].map((String value){
+            return DropdownMenuItem<String>(
+              value:value,
+              child: Text(value),
+            );
+          }).toList(),
+          onChanged: (String? newValue) {
+            setState(() {
+              selectedValue = newValue!;
+            });
+          },
+          ),
       ),
     );
   }
